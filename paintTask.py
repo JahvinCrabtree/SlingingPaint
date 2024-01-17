b08ffcb7cdbb50f9e4c0757150a17dc508c598d4
JahvinCrabtree <JahvinCrabtree@hotmail.com>
2024-01-17 21:54:05 +0000

    Its lit

---
+++ paintTask.py
@@ -0,0 +1,11 @@
+import math #Numbers and that.
+
+def calculateArea(length, width):
+    return length * width
+
+def calculateAreaCircle(radius):
+    return math.pi * radius**2 # ** 2 means squared. 
+
+height = float(input("Please enter the height of your wall to a single decimal place "))
+length = float(input("Please enter the length of your wall to a single decimal place "))
+
